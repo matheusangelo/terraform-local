462d3591f5f9113de1879a8e4a6eaed7fc9afc40
matheusangelo <matheusangelo10@gmail.com>
2024-01-01 20:39:24 -0300

    init

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  access_key                  = "test"
+  secret_key                  = "test"
+  region                      = "us-east-1"
+  s3_use_path_style           = false
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+}
+
+resource "aws_s3_bucket" "bucketapp" {
+  bucket = "app-bucket"
+}
+
+
